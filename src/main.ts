3e37d72e90ac116664dd71a20e7bf1c14da866e9
heiwa4126 <heiwa4126@gmail.com>
2025-10-27 16:20:43 +0900

    update

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { greet } from "./index.js";
 
 // 注意: パッケージ内でimportする場合、ESM環境では拡張子が必要です。
 // TypeScriptでは `import "./index.js"` と書いても実際にはindex.tsが読まれます。
-// これは TypeScript の仕様です。バグじゃありません。
+// これは TypeScript(tsc) の仕様です。バグじゃありません。
 // Bun や Viteでは `import { greet } from "./index` でうまいこと処理します。
 // Deno では `import { greet } from "./index.ts` でないとダメです。
 
